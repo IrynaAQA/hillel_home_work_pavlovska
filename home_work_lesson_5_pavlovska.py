1c25ee1085f88f95f612b949e05db80fb3229b32
IrynaAQA <pavlovska9719@gmail.com>
2023-03-06 18:15:46 +0200

    Add hw_4

---
+++ home_work_lesson_5_pavlovska.py
@@ -0,0 +1,13 @@
+# 1
+first_list = [1, 'one', 5, 'y', 3, 0, 2, 8, 'a']
+second_list =  [0, 1, 2, 3, 4, 5, 0, 'a', 0, 1, 2, 3]
+first_set = set(first_list)
+second_set = set(second_list)
+print(first_set.intersection(second_set))
+
+#2
+students =
+
+#3
+list_test = [1, 2, 3, 4, 5, 6, 1, 1, 8, 8, 9, 5, 4, 7, 6, 10, 7]
+
